37577de40f7ec686701b2e55b506c76db91f2d6d
paresiqbal <thecrowdel@gmail.com>
2023-09-25 21:03:27 +0700

    Adding comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Dropdown for navbar
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,12 +8,12 @@ function myFunction() {
   }
 }
 
-// JavaScript to clone and insert articles
+// Looping article
 const blogContainer = document.getElementById("blogContainer");
 const initialArticle = blogContainer.querySelector(".blog");
 
 for (let i = 2; i <= 3; i++) {
-  const clonedArticle = initialArticle.cloneNode(true); // Clone the initial article
-  clonedArticle.querySelector("h2").textContent = `Blog ${i}`; // Modify the heading text
-  blogContainer.appendChild(clonedArticle); // Append the cloned article to the container
+  const clonedArticle = initialArticle.cloneNode(true);
+  clonedArticle.querySelector("h2").textContent = `Blog ${i}`;
+  blogContainer.appendChild(clonedArticle);
 }
