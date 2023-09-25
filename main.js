fd890e8c73ca3e0455f7d0e1796b72a1bec3d1ae
paresiqbal <thecrowdel@gmail.com>
2023-09-25 21:02:52 +0700

    Add article

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+// JavaScript to clone and insert articles
+const blogContainer = document.getElementById("blogContainer");
+const initialArticle = blogContainer.querySelector(".blog");
+
+for (let i = 2; i <= 3; i++) {
+  const clonedArticle = initialArticle.cloneNode(true); // Clone the initial article
+  clonedArticle.querySelector("h2").textContent = `Blog ${i}`; // Modify the heading text
+  blogContainer.appendChild(clonedArticle); // Append the cloned article to the container
+}
